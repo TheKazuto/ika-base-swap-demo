4c0dfba8a6edba92515676ba3189ec1b9317a866
TheKazuto <watarizangado@gmail.com>
2025-12-08 16:34:21 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { WalletProvider } from '@mysten/wallet-kit'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { SuiClientProvider, getFullnodeUrl, WalletProvider } from '@mysten/dapp-kit';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WalletProvider autoConnect>
-      <App />
-    </WalletProvider>
+    <SuiClientProvider networks={{ mainnet: { url: getFullnodeUrl('mainnet') } }}>
+      <WalletProvider autoConnect>
+        <App />
+      </WalletProvider>
+    </SuiClientProvider>
   </React.StrictMode>,
-)
+);
